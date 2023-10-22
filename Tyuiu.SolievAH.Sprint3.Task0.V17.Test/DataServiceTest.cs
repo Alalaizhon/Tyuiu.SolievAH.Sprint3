748af6037ed3594420d27afddcd37fd2a0cdcb67
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-10-22 18:55:59 +0500

    Add project files.

---
+++ Tyuiu.SolievAH.Sprint3.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolievAH.Sprint3.Task0.V17.Lib;
+
+namespace Tyuiu.SolievAH.Sprint3.Task0.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double res = ds.GetSumSeries(1, 10);
+
+            Assert.AreEqual(-0.42, res);
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint3.Task0.V17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SolievAH.Sprint3.Task0.V17.Lib;
+
+namespace Tyuiu.SolievAH.Sprint3.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Солиев А.Х. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: 3.0 циклы                                                         *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Солиев А.Х.  | СМАРТб-23-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле: cos i * 0,5                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue, stopValue;
+            Console.WriteLine("Введите начало и конец цикла: ");
+            startValue = Convert.ToInt32(Console.ReadLine());
+            stopValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.GetSumSeries(startValue, stopValue);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
